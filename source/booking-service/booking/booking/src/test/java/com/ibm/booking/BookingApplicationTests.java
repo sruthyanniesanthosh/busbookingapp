816c45472005bb886b9b22ad2cbf3162301eddb9
Sruthy Santhosh <srsanth1@in.ibm.com>
2019-12-10 12:19:19 +0530

    first commit

---
+++ source/booking-service/booking/booking/src/test/java/com/ibm/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
